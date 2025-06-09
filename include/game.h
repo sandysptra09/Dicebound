cd5862a79186c84165f17a506ceeb1edf80a1262
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2025-06-09 12:04:32 +0700

    feat: update queue initialize

--- include/game.h
+++ include/game.h
@@ -1,6 +1,26 @@
 #ifndef GAME_H
 #define GAME_H
 
+#define MAX_QUEUE 10
+
+struct Queue
+{
+    int data[MAX_QUEUE];
+    int front;
+    int rear;
+    int top;
+};
+
+extern Queue turnQueue;
+
+void createQueue();
+bool isEmpty();
+bool isFull();
+void inQueue(int val);
+int deQueue();
+void displayQueue();
+int find(int val);
+
 void determineTurnOrder();
 bool playMinigame(int playerIndex);
 void playTurn();
